edcc1a36bd5c389a4b1603a13fecc4e73a23f33e
chakib <chakibdaii1995@gmail.com>
2026-05-25 12:31:02 +0200

    updated email

--- src/components/commands/Email.tsx
+++ src/components/commands/Email.tsx
@@ -9,13 +9,23 @@ const Email: React.FC = () => {
   /* ===== get current command ===== */
   const currentCommand = _.split(history[0], " ");
 
+  const email = "chakiibdaii@gmail.com";
+
   if (rerender && currentCommand[0] === "email" && currentCommand.length <= 1) {
-    window.open("mailto:" + "contact@satnaing.dev", "_self");
+    window.open(`mailto:${email}`, "_self");
   }
 
   return (
     <Wrapper>
-      <span>contact@satnaing.dev</span>
+      <p>
+        📧 Email: <a href={`mailto:${email}`}>{email}</a>
+      </p>
+
+      <p>
+        Feel free to reach out for collaborations, open-source projects,
+        enterprise software engineering opportunities, or discussions around
+        distributed systems, BPM, and developer tooling.
+      </p>
     </Wrapper>
   );
 };
